182ad96fdc51ada88c7971e3e0b7fe839cd5b5b6
Bear <Bear@Erins-MacBook-Air-2.local>
2020-11-27 20:42:21 -0500

    updated styling

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect('mongodb://localhost/movies', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
